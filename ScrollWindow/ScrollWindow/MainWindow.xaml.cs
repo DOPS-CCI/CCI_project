76cce87cb5c3ba1f4d2b22fd5f13a58fc93d9524
James E Lenz <jlenz@virginia.edu>
2012-11-13 23:27:51 -0500

    Further development
    Scroll geometry close to correct; added Event tracking

--- ScrollWindow/ScrollWindow/MainWindow.xaml.cs
+++ ScrollWindow/ScrollWindow/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace ScrollWindow
         double YScaleUnitsToInches;
         double currentDisplayWidthInSecs = 10D;
         double currentDisplayOffsetInSecs = -5D;
+        Dictionary<int, Event.InputEvent> events = new Dictionary<int, Event.InputEvent>();
+        BDFEDFFileReader bdf;
         public MainWindow()
         {
             OpenFileDialog dlg = new OpenFileDialog();
@@ -48,7 +50,7 @@ namespace ScrollWindow
             Header.Header head = (new HeaderFileReader(dlg.OpenFile())).read();
             EventDictionary.EventDictionary ED = head.Events;
 
-            BDFEDFFileReader bdf = new BDFEDFFileReader(
+            bdf = new BDFEDFFileReader(
                 new FileStream(System.IO.Path.Combine(directory, head.BDFFile),
                     FileMode.Open, FileAccess.Read));
             int samplingRate = bdf.NSamp / bdf.RecordDuration;
@@ -60,7 +62,6 @@ namespace ScrollWindow
             {
                 if (!ede.intrinsic) channelList.Add(bdf.ChannelNumberFromLabel(ede.channelName));
             }
-            channelList.Add(bdf.NumberOfChannels - 1);
 
             InitializeComponent();
 
@@ -69,37 +70,75 @@ namespace ScrollWindow
                 BDFChannelGraph pg = new BDFChannelGraph(bdf, i);
                 GraphCanvas.Children.Add(pg);
             }
+            GraphCanvas.Children.Add(new BDFChannelGraph(bdf, bdf.NumberOfChannels - 1, 100D));
 
+            Event.EventFactory.Instance(head.Events); // set up the factory
+            EventFileReader efr = new EventFileReader(
+                new FileStream(System.IO.Path.Combine(directory, head.EventFile),
+                    FileMode.Open, FileAccess.Read)); // open Event file
+            foreach (Event.InputEvent ie in efr)// read in all Events into dictionary
+            {
+                if (!events.ContainsKey(ie.GC)) //quietly skip duplicates
+                    events.Add(ie.GC, ie);
+            }
+            efr.Close();
         }
 
         private void ScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            double w =e.PreviousSize.Width;
-            double h = e.PreviousSize.Height - ScrollBarSize;
             if (e.HeightChanged)
             {
-                h = e.NewSize.Height - ScrollBarSize;
+                double h = e.NewSize.Height - ScrollBarSize;
                 IndexLine.Y2 = h;
                 YScaleUnitsToInches = h / GraphCanvas.ActualHeight;
             }
             if (e.WidthChanged)
             {
-                w = e.NewSize.Width - ScrollBarSize;
+                double w = e.NewSize.Width - ScrollBarSize;
                 IndexLine.X1 = IndexLine.X2 = w / 2;
                 XScaleSecsToInches = w / currentDisplayWidthInSecs;
             }
 
             GraphCanvas.LayoutTransform = new ScaleTransform(XScaleSecsToInches,
-                YScaleUnitsToInches, currentDisplayWidthInSecs / 2D, 0D); //set initial transform to fill Viewer plot
-            RedrawGraphicCanvas();
+                YScaleUnitsToInches, currentDisplayOffsetInSecs + currentDisplayWidthInSecs / 2D, 0D);
         }
 
         private void Viewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
         {
             double loc = e.HorizontalOffset;
-            Loc.Text = (loc/XScaleSecsToInches).ToString("0.00");
             currentDisplayOffsetInSecs = loc / XScaleSecsToInches;
+            double midPoint = currentDisplayOffsetInSecs + currentDisplayWidthInSecs / 2D;
+            Loc.Text = midPoint.ToString("0.00");
             RedrawGraphicCanvas();
+
+            BDFPoint mid = new BDFPoint(bdf);
+            mid.FromSecs(midPoint);
+            int sample = bdf.getStatusSample(mid);
+            InputEvent ie;
+            bool r = events.TryGetValue(sample, out ie);
+            if (r)
+                EventPastInfo.Text = ie.ToString();
+            else
+                EventPastInfo.Text = "No Event";
+            int past = sample;
+            for (BDFPoint p = mid; ; mid++)
+            {
+                sample = bdf.getStatusSample(p);
+                if (sample != past)
+                {
+                    if (sample != int.MinValue)
+                    {
+                        r = events.TryGetValue(sample, out ie);
+                        if (r)
+                        {
+                            EventNextInfo.Text = ie.ToString();
+                            return;
+                        }
+                    }
+                    EventNextInfo.Text = "No Event";
+                    return;
+                }
+            }
         }
 
         bool InDrag = false;
@@ -125,20 +164,21 @@ namespace ScrollWindow
         private void Viewer_MouseMove(object sender, MouseEventArgs e)
         {
             if (!InDrag) return;
-            Viewer.ScrollToHorizontalOffset(startDragScrollLocation+e.GetPosition(Viewer).X-startDragMouseLocation.X);
+            Viewer.ScrollToHorizontalOffset(startDragScrollLocation - e.GetPosition(Viewer).X + startDragMouseLocation.X);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button b = (Button)sender;
             double d = Convert.ToDouble(b.Content);
+            double oldDisplayWidthInSecs = currentDisplayWidthInSecs;
             currentDisplayWidthInSecs /= d;
-            XScaleSecsToInches /= d;
+            XScaleSecsToInches *= d;
             TransformGroup t = new TransformGroup();
             t.Children.Add(GraphCanvas.LayoutTransform);
             t.Children.Add(new ScaleTransform(d, 1, Viewer.ContentHorizontalOffset + Viewer.ViewportWidth / 2, 0D));
             GraphCanvas.LayoutTransform = new MatrixTransform(t.Value);
-            Viewer.ScrollToHorizontalOffset(currentDisplayOffsetInSecs + currentDisplayWidthInSecs / 2);
+            Viewer.ScrollToHorizontalOffset(XScaleSecsToInches * (currentDisplayOffsetInSecs + (oldDisplayWidthInSecs - currentDisplayWidthInSecs) / 2D));
         }
 
         private void RedrawGraphicCanvas(){
@@ -158,17 +198,29 @@ namespace ScrollWindow
 
     public class BDFChannelGraph : Canvas
     {
-        const double CanvasHeight = 200D;
+        double CanvasHeight = 200D;
         int _channel;
         BDFEDFFileReader _BDF;
         double BrushScale;
-        public BDFChannelGraph(BDFEDFFileReader BDF, int channelNumber): base()
+        public BDFChannelGraph(BDFEDFFileReader BDF, int channelNumber)
+            : base()
         {
             _channel = channelNumber;
             _BDF = BDF;
             this.Height = CanvasHeight;
             this.Width = (double)(BDF.RecordDuration * BDF.NumberOfRecords); //NOTE: always scaled in seconds
-            BrushScale = ((double)BDF.RecordDuration) / ((double)(BDF.NSamp));
+            BrushScale = ((double)BDF.RecordDuration) / ((double)(3 * BDF.NSamp));
+        }
+
+        public BDFChannelGraph(BDFEDFFileReader BDF, int channelNumber, double height)
+            : base()
+        {
+            CanvasHeight = height;
+            _channel = channelNumber;
+            _BDF = BDF;
+            this.Height = CanvasHeight;
+            this.Width = (double)(BDF.RecordDuration * BDF.NumberOfRecords); //NOTE: always scaled in seconds
+            BrushScale = ((double)BDF.RecordDuration) / ((double)(3 * BDF.NSamp));
         }
 
         public void reDraw(double lowSecs, double highSecs)
@@ -196,10 +248,11 @@ namespace ScrollWindow
             double scale = CanvasHeight / (lowValue - hiValue);
             System.Windows.Shapes.Path path = new System.Windows.Shapes.Path();
             path.Stroke = Brushes.Black;
-            path.StrokeThickness = Math.Sqrt(highSecs - lowSecs) * BrushScale;
+            path.StrokeThickness = Math.Max((highSecs - lowSecs) * BrushScale, 0.002);
             StreamGeometry geometry = new StreamGeometry();
             StreamGeometryContext ctx = geometry.Open();
-            int decimate = Convert.ToInt32((highSecs - lowSecs) * _BDF.NSamp / 1024D);
+            List<Point> pointList = new List<Point>();
+            int decimate = Convert.ToInt32((highSecs - lowSecs) * _BDF.NSamp / 1536D);
             if (decimate < 1) decimate = 1;
             ctx.BeginFigure(new Point(lowSecs, scale * (_BDF.getSample(_channel, low) - offset) + CanvasHeight / 2D), false, false);
             for (BDFPoint i = low + 1; i.lessThan(hi); )
@@ -215,14 +268,16 @@ namespace ScrollWindow
                         max = Math.Max(max, sample);
                         min = Math.Min(min, sample);
                     }
-                    ctx.LineTo(new Point(i.ToSecs(), scale * (max - offset) + CanvasHeight / 2D), true, false);
-                    ctx.LineTo(new Point(i.ToSecs(), scale * (min - offset) + CanvasHeight / 2D), true, false);
+                    pointList.Add(new Point(i.ToSecs(), scale * (max - offset) + CanvasHeight / 2D));
+                    if (max != min)
+                        pointList.Add(new Point(i.ToSecs(), scale * (min - offset) + CanvasHeight / 2D));
                 }
                 else
                 {
                     i.Increment(decimate);
                 }
             }
+            ctx.PolyLineTo(pointList, true, true);
             ctx.Close();
             geometry.Freeze();
             path.Data = geometry;
