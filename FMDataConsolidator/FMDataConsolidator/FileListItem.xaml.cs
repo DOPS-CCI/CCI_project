773e7e5bd6ebee28780d984e36f41689ab486430
James E Lenz <jlenz@virginia.edu>
2014-02-14 14:41:50 -0500

    First commit of new data consolidator

---
+++ FMDataConsolidator/FMDataConsolidator/FileListItem.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using FILMANFileStream;
+
+namespace FMDataConsolidator
+{
+    /// <summary>
+    /// Interaction logic for FileListItem.xaml
+    /// </summary>
+    public partial class FileListItem : ListBoxItem
+    {
+        static int fileUID = 0;
+        FILMANFileRecord FFR;
+        int FileUID;
+        int numberOfRecords;
+        ObservableCollection<PointGroup> _PointGroups = new ObservableCollection<PointGroup>();
+        ObservableCollection<GroupVar> _GroupVars = new ObservableCollection<GroupVar>();
+
+        public FileListItem(FILMANFileRecord ffr)
+        {
+            InitializeComponent();
+
+            FileUID = ++fileUID;
+            FFR = ffr;
+            FileName.Text = ffr.path;
+            FILMANInputStream fis = ffr.stream;
+
+            ToolTip tt = new ToolTip();
+            string s = fis.Description(0);
+            StringBuilder sb = new StringBuilder(s); //assume first line that isn't blank
+            for (int i = 1; i < 6; i++)
+            {
+                s = fis.Description(i);
+                if (s != "")
+                    sb.Append(Environment.NewLine + s);
+            }
+            tt.Content = sb.ToString();
+            FileName.ToolTip = tt;
+            numberOfRecords = fis.NR;
+            int ng = fis.NG;
+            for (int i = 2; i < ng; i++)
+            {
+                _GroupVars.Add(new GroupVar
+                {
+                    FM_GVName =fis.GVNames(i).Trim(),
+                    GVName = fis.GVNames(i),
+                    Format = NSEnum.String
+                });
+            }
+
+            AddNewPointGroup();
+
+        }
+
+        public ObservableCollection<PointGroup> PointGroups { get { return _PointGroups; } }
+        public ObservableCollection<GroupVar> GroupVars { get { return _GroupVars; } }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewPointGroup();
+            RemovePointSelection.IsEnabled = true;
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            int pg = this.Points.SelectedIndex;
+            if (pg < 0) return;
+            _PointGroups.RemoveAt(pg);
+            if (_PointGroups.Count <= 1) RemovePointSelection.IsEnabled = false;
+        }
+
+        private void AddNewPointGroup()
+        {
+            FILMANInputStream fis = FFR.stream;
+            int nc = fis.NC;
+            int np = fis.NP;
+            _PointGroups.Add(new PointGroup("{1-" + nc.ToString("0") + "}[1-" + np.ToString("0") + "]",
+                "F" + FileUID.ToString("0") + "C%cP%p"));
+        }
+
+        private void Points_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            FrameworkElement f = e.EditingElement;
+        }
+    }
+
+    public class PointGroup
+    {
+        public string SelectionString { get; set; }
+        List<SelectedPoint> SelectedPoints;
+        public string Name { get; set; }
+
+        public PointGroup(string selection, string name)
+        {
+            SelectionString = selection;
+            Name = name;
+        }
+    }
+
+    struct SelectedPoint
+    {
+        int RecordNumber;
+        int PointNumber;
+    }
+
+    public class GroupVar{
+        public string FM_GVName { get; set; }
+        string _GVName;
+        public string GVName
+        {
+            get
+            {
+                return _GVName;
+            }
+            set
+            {
+                _GVName = value.Substring(0, Math.Min(12, value.Length)); //limit length to 12
+            }
+        }
+        public NSEnum Format { get; set; }
+    }
+
+    public enum NSEnum { Number, String }
+}
---
+++ FMDataConsolidator/FMDataConsolidator/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using FILMANFileStream;
+using SYSTATFileStream;
+
+
+namespace FMDataConsolidator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<FILMANFileRecord> FILMANFileRecords = new List<FILMANFileRecord>(1);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFile_Click(object sender, RoutedEventArgs e)
+        {
+            FILMANFileRecord ffr;
+            if ((ffr = OpenFILMANFile()) == null) return;
+            FileListItem fli = new FileListItem(ffr);
+            Files.Items.Add(fli);
+            ffr.stream.Description(0);
+            if (Files.Items.Count > 1) RemoveFile.IsEnabled = true;
+        }
+
+        private void RemoveFile_Click(object sender, RoutedEventArgs e)
+        {
+            object fli = Files.SelectedItem;
+            if (fli == null) return;
+            Files.Items.Remove(fli);
+            if (Files.Items.Count <= 1) RemoveFile.IsEnabled = false;
+        }
+
+        private FILMANFileRecord OpenFILMANFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open a FILMAN file ...";
+            ofd.AddExtension = true;
+            ofd.DefaultExt = ".fmn"; // Default file extension
+            ofd.Filter = "FILMAN files (.fmn)|*.fmn|All files|*.*"; // Filter files by extension
+            Nullable<bool> result = ofd.ShowDialog();
+            if (result == false) return null;
+
+            FILMANInputStream fmTemp;
+            try
+            {
+                fmTemp = new FILMANInputStream(new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to access FILMAN file " + ofd.FileName + "." + Environment.NewLine + "Exception: " + ex.Message,
+                    "FILMAN error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+            FILMANFileRecord ffr = new FILMANFileRecord();
+            ffr.path = ofd.FileName;
+            ffr.stream = fmTemp;
+            return ffr;
+        }
+
+        private string OpenSYSTATFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Create a SYSTAT file ...";
+            sfd.AddExtension = false;
+            sfd.OverwritePrompt = false; //we'll ask later if file duplicate, when we know final format
+            sfd.DefaultExt = ".sys"; // Default file extension
+            sfd.Filter = "SYSTAT files (.sys or .syd)|*.sys;*.syd|All files|*.*"; // Filter files by extension
+            Nullable<bool> result = sfd.ShowDialog();
+            if (result == false) return "SYSTATfile";
+            return Path.ChangeExtension(sfd.FileName, GetCurrentSYSTATExtension());
+        }
+
+        private string GetCurrentSYSTATExtension()
+        {
+            if (SYS == null) return "sys";
+            if ((bool)SYS.IsChecked) return "sys";
+            else return "syd";
+        }
+
+        private void BrowseSYSTAT_Click(object sender, RoutedEventArgs e)
+        {
+            SYSTATFileName.Text = OpenSYSTATFile();
+        }
+
+        private void Format_Checked(object sender, RoutedEventArgs e)
+        {
+            SYSTATFileName.Text = Path.ChangeExtension(SYSTATFileName.Text, GetCurrentSYSTATExtension());
+        }
+
+        private void SYSTATFileName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SYSTATFileName.Text = Path.ChangeExtension(SYSTATFileName.Text, GetCurrentSYSTATExtension());
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+    }
+
+    public class FILMANFileRecord
+    {
+        public FILMANInputStream stream { get; internal set; }
+        public string path { get; internal set; }
+    }
+}
