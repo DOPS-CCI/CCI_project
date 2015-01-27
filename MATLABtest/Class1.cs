6eaf5fa91452099481cc96118201c0cba77a720e
James E Lenz <jlenz@virginia.edu>
2015-01-27 10:57:15 -0500

    Test class for MATLAB communication

---
+++ MATLABtest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MATLABtest
+{
+    public class Class1
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        static public double C { get; set; }
+        public double[,] D = new double[20,20];
+        public Class1()
+        {
+            C++;
+        }
+    }
+}
