fcd264700e83e223b36aebc437dba59ab88cb256
James E Lenz <jlenz@virginia.edu>
2015-04-06 11:37:14 -0400

    Further testing of MATLAB link

--- MATLABtest/Class1.cs
+++ MATLABtest/Class1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace MATLABtest
 {
@@ -10,10 +11,23 @@ namespace MATLABtest
         public double A { get; set; }
         public double B { get; set; }
         static public double C { get; set; }
-        public double[,] D = new double[20,20];
+        public double[,] D;
         public Class1()
         {
             C++;
+            D = new double[20,20];
+        }
+        public void ShowMess(string message)
+        {
+            MessageBox.Show(message + "C = " + C.ToString("0"));
+        }
+        public double sumD()
+        {
+            double d=0;
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    d += D[i, j];
+            return d;
         }
     }
 }
