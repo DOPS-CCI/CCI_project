c0a11a44671bfc93695a08fffd08364ec7d2029c
James E Lenz <jlenz@virginia.edu>
2015-04-17 13:15:57 -0400

    Additions to CCIUtilities
    Matrix, vector manipulation; analytic roots to polynomials; polynomial
    fitting, LM non-linear fitting

---
+++ CCIUtilities/LevenbergMarquardt.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using LinearAlgebra;
+
+namespace CCIUtilities
+{
+
+    public class LevenbergMarquardt
+    {
+        public delegate NVector Function(NVector t, NVector p);
+        public delegate NMMatrix JFunc(NVector t, NVector p);
+
+        const double lambda_DN_fac = 2D;
+        const double lambda_UP_fac = 3D;
+        Function func;
+        JFunc Jfunc;
+        NVector p;
+        NVector t;
+        NVector y_dat;
+        NVector dp;
+        NVector p_min;
+        NVector p_max;
+        double[] eps;
+        UpdateType updateType;
+        int MaxIter;
+        int n;
+        int m;
+
+        NMMatrix J;
+        NMMatrix JtJ;
+        NVector Jtdy;
+        double DOF;
+        double lambda;
+        double X2;
+        double dX2;
+        NVector p_old;
+        NVector y_old;
+        NVector y_hat;
+        double alpha;
+        double nu;
+        int iteration;
+
+        int _result = 0;
+        public ResultType Result
+        {
+            get { return (ResultType)_result; }
+        }
+
+        public int Iterations
+        {
+            get { return iteration; }
+        }
+
+        public double ChiSquare
+        {
+            get { return X2 / DOF; }
+        }
+
+        public double normalizedStandardErrorOfFit
+        {
+            get
+            {
+                return (X2 / DOF - DOF) / Math.Sqrt(2 * DOF);
+            }
+        }
+
+        public NMMatrix parameterCovariance
+        {
+            get
+            {
+                NMMatrix Vp = NMMatrix.I(n) / JtJ;
+                return Vp;
+            }
+        }
+
+        public NVector parameterStandardError
+        {
+            get
+            {
+                NVector Sp = (DOF * (NMMatrix.I(n) / JtJ).Diag()).Apply((LinearAlgebra.F)Math.Sqrt);
+                return Sp;
+            }
+        }
+
+        public LevenbergMarquardt(Function func, JFunc Jfunc, NVector p_min, NVector p_max, NVector dp, double[] eps, UpdateType updateType)
+        {
+            this.func = func;
+            this.Jfunc = Jfunc;
+            n = p_min.N;
+            this.p_min = p_min;
+            if (p_max.N != n) throw new Exception("LevenbergMarquardt: size mismatch p_max");
+            this.p_max = p_max;
+            if (Jfunc == null)
+            {
+                if (dp.N != n) throw new Exception("LevenbergMarquardt: size mismatch dp");
+                this.dp = dp;
+            }
+            MaxIter = 50 * n;
+            this.eps = eps;
+            this.updateType = updateType;
+        }
+
+        public NVector Calculate(NVector par_initial, NVector t, NVector y_dat)
+        {
+            _result = 0;
+            m = t.N;
+            if (par_initial.N != n) throw new Exception("LevenbergMarquardt.Calculate: size mismatch parms");
+            this.p = par_initial;
+            this.t = t;
+            if (y_dat.N != m) throw new Exception("LevenbergMarquardt.Calculate: size mismatch t-y");
+            this.y_dat = y_dat;
+
+//            weight_sq = (m - n + 1) / y_dat.Dot(y_dat);
+            DOF = (double)(m - n + 1);
+
+            //initalize Jacobian and related matrices
+            y_hat = func(t, p);
+            y_old = y_hat;
+            if (Jfunc == null)
+                J = Jacobian(p, y_hat);
+            else
+                J = Jfunc(t, p);
+            NVector delta_y = y_dat - y_hat;
+            X2 = delta_y.Dot(delta_y);
+            JtJ = J.Transpose() * J;
+            Jtdy = J.Transpose() * delta_y;
+
+            iteration = 0;
+
+            if (Jtdy.Abs().Max() < eps[0])
+            {
+                _result = 1;
+                return p; //Good guess!!!
+            }
+            if (updateType == UpdateType.Marquardt)
+                lambda = 0.01D;
+            else
+                lambda = 0.01D * JtJ.Diag().Max();
+
+            bool stop = false;
+
+            /************************** Begin Main loop ***********************/
+            // y_hat = vector of y estimates for current value of parameters
+            // y_try = vector of y estimates for current trial value of parameters
+            // y_dat = given dependent values (fixed)
+            // y_old = vector of y estimates for previous value of parameters (used in Broyden estimate of J)
+            // t = given independent values (fixed)
+            // p = current accepted estimate of parameters
+            // h = last calculated (trial) increment for the parameters
+            // p_try = current trial value for the parameters
+            // p_old = previous accepted value of parameters (used in Broyden estimate of J)
+            // X2 = chi^2 of last accepted estimate
+            // X2_try = chi^2 of current trial estimate
+            // J = current estimate of Jacobian at p
+
+            while (!stop)
+            {
+                iteration++;
+
+                NVector h;
+                if (updateType == UpdateType.Marquardt)
+                    h = Jtdy / (JtJ + lambda * JtJ.Diag().Diag());
+                else
+                    h = Jtdy / (JtJ + lambda * NMMatrix.I(n));
+
+                NVector p_try = (p + h).Max(p_min).Min(p_max);
+
+                NVector y_try = func(t, p_try);
+                delta_y = y_dat - y_try;
+
+                double X2_try = delta_y.Dot(delta_y);
+
+                if (updateType == UpdateType.Quadratic)
+                {
+                    alpha = Jtdy.Dot(h) / ((X2_try - X2) / 2D + 2D * Jtdy.Dot(h));
+                    h = h * alpha;
+                    p_try = (p_try + h).Max(p_min).Min(p_max);
+                    delta_y = y_dat - func(t, p_try);
+                    X2_try = delta_y .Dot(delta_y);
+                }
+                dX2 = X2_try - X2;
+
+                double rho = -dX2 / (2D * (lambda * h + Jtdy).Dot(h));
+
+                if (dX2 < 0D) //found a better estimate
+                {
+                    X2 = X2_try;
+                    p_old = p;
+                    p = p_try;
+                    y_old = y_hat;
+                    y_hat = y_try;
+
+                    if (iteration % (2 * n) == 0) //|| dX2 > 0 or is it rho > ep[3] ?
+                        if (Jfunc == null)
+                            J = Jacobian(p, y_hat);
+                        else
+                            J = Jfunc(t, p);
+                    else
+                        J = J + (y_hat - y_old - J * h).Cross(h) / h.Dot(h); //Broyden rank-1 update of J
+
+                    JtJ = J.Transpose() * J;
+                    Jtdy = J.Transpose() * delta_y;
+
+                    switch (updateType)
+                    {
+                        case UpdateType.Marquardt:
+                            lambda = Math.Max(lambda / lambda_DN_fac, 1E-7);
+                            break;
+                        case UpdateType.Quadratic:
+                            lambda = Math.Max(lambda / (1 + alpha), 1E-7);
+                            break;
+                        case UpdateType.Nielsen:
+                            lambda = lambda * Math.Max(1D / 3D, 1D - Math.Pow(2D * rho - 1D, 3));
+                            nu = 2D;
+                            break;
+                    }
+
+                    if (Jtdy.Abs().Max() < eps[0] && iteration > 2)
+                    {
+                        _result = 1;
+                        stop = true;
+                    }
+                    else if ((h / p).Abs().Max() < eps[1] && iteration > 2)
+                    {
+                        _result = 2;
+                        stop = true;
+                    }
+                    else if (X2 / (m - n + 1) < eps[2] && iteration > 2)
+                    {
+                        _result = 3;
+                        stop = true;
+                    }
+                }
+                else //Not a better estimate
+                {
+                    if (iteration % (2 * n) == 0) //update J every 2n th no matter what
+                    {
+                        if (Jfunc == null)
+                            J = Jacobian(p, y_hat);
+                        else
+                            J = Jfunc(t, p);
+                        JtJ = J.Transpose() * J;
+                        Jtdy = J.Transpose() * (y_dat - y_hat);
+                    }
+
+                    switch (updateType)
+                    {
+                        case UpdateType.Marquardt:
+                            lambda = Math.Min(lambda * lambda_UP_fac, 1E7);
+                            break;
+                        case UpdateType.Quadratic:
+                            lambda = lambda + Math.Abs(dX2 / (2D * alpha));
+                            break;
+                        case UpdateType.Nielsen:
+                            lambda = lambda * nu;
+                            nu *= 2D;
+                            break;
+                    }
+                }
+
+                if (iteration > MaxIter && !stop)
+                {
+                    _result = -1;
+                    return p;
+                }
+            }
+            /************************** End Main loop ************************/
+
+            return p;
+        }
+
+        private NMMatrix Jacobian(NVector p, NVector y)
+        {
+            NVector ps = new NVector(p); //save a copy
+            NMMatrix J = new NMMatrix(m, n); //creating a new J from scratch
+            double del_p;
+            for (int j = 0; j < n; j++)
+            {
+                del_p = Math.Max(dp[j] * Math.Abs(p[j]), dp[j]);
+                p[j] = ps[j] + del_p;
+                NVector y1 = func(t, p);
+                if (dp[j] != 0D) //forward or backward difference
+                    J.ReplaceColumn(j, (y1 - y) / del_p);
+                else //central difference
+                {
+                    p[j] = ps[j] - del_p;
+                    J.ReplaceColumn(j, (y1 - func(t, p)) / (2D * del_p));
+                }
+                p[j] = ps[j]; //restore this value
+            }
+            return J;
+        }
+
+        private NMMatrix Broyden(NVector p_old, NVector y_old, NMMatrix J, NVector p, NVector y)
+        {
+            NVector h = p - p_old;
+            J = J + (y - y_old - J * h).Cross(h) / h.Dot(h);
+            return J;
+        }
+
+        public enum UpdateType { Marquardt, Quadratic, Nielsen };
+
+        public enum ResultType { MaximumIterations = -1, NoResult = 0, Jacobian = 1, ParameterChange = 2, ChiSquare = 3 };
+    }
+}
---
+++ CCIUtilities/NMMatrix.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Text;
+
+namespace LinearAlgebra
+{
+    public delegate double F(double e);
+
+    public class NMMatrix
+    {
+        double[,] _matrix;
+        int _n;
+        int _m;
+        bool _transpose = false;
+
+        public int N
+        {
+            get
+            {
+                if (_transpose)
+                    return _m;
+                else
+                    return _n;
+            }
+        }
+
+        public int M
+        {
+            get
+            {
+                if (_transpose)
+                    return _n;
+                else
+                    return _m;
+            }
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                if (_transpose)
+                    return _matrix[j, i];
+                else
+                    return _matrix[i, j];
+            }
+            set
+            {
+                if (_transpose)
+                    _matrix[j, i] = value;
+                else
+                    _matrix[i, j] = value;
+            }
+        }
+
+        public NMMatrix(int N, int M)
+        {
+            _matrix = new double[N, M];
+            _n = N;
+            _m = M;
+        }
+
+        public NMMatrix(double[,] A)
+        {
+            _n = A.GetLength(0);
+            _m = A.GetLength(1);
+            _matrix = new double[_n, _m];
+            for (int i = 0; i < _n; i++)
+                for (int j = 0; j < _m; j++)
+                    _matrix[i, j] = A[i, j];
+        }
+
+        public NMMatrix(NMMatrix A) //copy constructor
+        {
+            _matrix = new double[A.N, A.M];
+            _n = A.N;
+            _m = A.M;
+            for (int i = 0; i < A.N; i++)
+                for (int j = 0; j < A.M; j++)
+                    _matrix[i, j] = A[i, j];
+        }
+
+        private NMMatrix()
+        {
+
+        }
+
+        public NMMatrix Transpose()
+        {
+            //make shallow copy, so both transposed and untransposed matrix may be used in same calculation
+            NMMatrix A = new NMMatrix();
+            A._matrix = _matrix;
+            A._m = _m;
+            A._n = _n;
+            A._transpose = !_transpose;
+            return A;
+        }
+
+        public static NMMatrix operator + (NMMatrix A, NMMatrix B)
+        {
+            if (A.N != B.N || A.M != B.M) throw new Exception("NMMatrix.Add: incompatable sizes");
+            NMMatrix C = new NMMatrix(A);
+            for (int i = 0; i < A.N; i++)
+                for (int j = 0; j < A.M; j++)
+                    C._matrix[i, j] += B[i, j];
+            return C;
+        }
+
+        public static NMMatrix operator -(NMMatrix A, NMMatrix B)
+        {
+            if (A.N != B.N || A.M != B.M) throw new Exception("NMMatrix.Add: incompatable sizes");
+            NMMatrix C = new NMMatrix(A);
+            for (int i = 0; i < A.N; i++)
+                for (int j = 0; j < A.M; j++)
+                    C._matrix[i, j] -= B[i, j];
+            return C;
+        }
+
+        public static NMMatrix operator *(NMMatrix A, NMMatrix B)
+        {
+            if (A.M != B.N ) throw new Exception("NMMatrix.Mul: incompatable sizes");
+            NMMatrix C = new NMMatrix(A.N, B.M);
+            for (int i = 0; i < A.N; i++)
+                for (int j = 0; j < B.M; j++)
+                {
+                    double c = 0D;
+                    for (int k = 0; k < A.M; k++)
+                        c += A[i, k] * B[k, j];
+                    C._matrix[i, j] = c;
+                }
+            return C;
+        }
+
+        public static NMMatrix operator /(NMMatrix A, double b)
+        {
+            NMMatrix C = new NMMatrix(A.N, A.M);
+            for (int i = 0; i < A.N; i++)
+                for (int j = 0; j < A.M; j++)
+                    C._matrix[i, j] = A[i, j] / b;
+            return C;
+        }
+
+        public static NMMatrix operator *(NMMatrix A, double b)
+        {
+            return b * A;
+        }
+
+        public static NMMatrix operator *(double a, NMMatrix B)
+        {
+            NMMatrix C = new NMMatrix(B);
+            for (int i = 0; i < B.N; i++)
+                for (int j = 0; j < B.M; j++)
+                    C._matrix[i, j] *= a;
+            return C;
+        }
+
+        public static NMMatrix I(int n)
+        {
+            NMMatrix A = new NMMatrix(n, n);
+            for (int i = 0; i < n; i++)
+                A._matrix[i, i] = 1D;
+            return A;
+        }
+
+        public NVector Diag()
+        {
+            if (_n != _m) throw new Exception("NMMatrix.Diag: non-square matrix");
+            NVector A = new NVector(_n);
+            for (int i = 0; i < _n; i++)
+                A[i] = _matrix[i, i];
+            return A;
+        }
+
+        public void ReplaceColumn(int col, NVector V)
+        {
+            if (col < 0 || col >= this.M) throw new Exception("NMMatrix.ReplaceColumn: invalid column number");
+            for (int j = 0; j < _n; j++)
+                this[j, col] = V[j];
+        }
+
+        public NVector ExtractColumn(int col)
+        {
+            if (col < 0 || col >= this.M) throw new Exception("NMMatrix.ReplaceColumn: invalid column number");
+            NVector V = new NVector(N);
+            for (int j = 0; j < _n; j++)
+                V[j] = this[j, col];
+            return V;
+        }
+
+        public NMMatrix ExtractMatrix(int col, int coln)
+        {
+            NMMatrix A = new NMMatrix(N, coln);
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < coln; j++)
+                    A[i, j] = this[i, col + j];
+            return A;
+        }
+
+        public NMMatrix Augment(NVector V)
+        {
+            if (N != V.N) throw new Exception("NMMatrix.Concatenate: incompatable sizes");
+            NMMatrix B = new NMMatrix(N, M + 1);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                    B[i, j] = this[i, j];
+                B[i, M] = V[i];
+            }
+            return B;
+        }
+
+        public NMMatrix Augment(NMMatrix A)
+        {
+            if (N != A.N) throw new Exception("NMMatrix.Concatenate: incompatable sizes");
+            NMMatrix B = new NMMatrix(N, M + A.M);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                    B[i, j] = this[i, j];
+                for (int j = 0; j < A.M; j++)
+                    B[i, M + j] = A[i, j];
+            }
+            return B;
+        }
+
+        public static NVector operator /(NVector A, NMMatrix B)
+        {
+            if (B.N != A.N) throw new Exception("NMMatrix.Div: incompatable matrix and vector sizes");
+
+            NMMatrix C = B.Augment(A);
+            C.GaussJordanElimination();
+
+            return C.ExtractColumn(C.M - 1);
+        }
+
+        public static NMMatrix operator /(NMMatrix A, NMMatrix B)
+        {
+            NMMatrix C = B.Augment(A);
+            C.GaussJordanElimination();
+
+            return C.ExtractMatrix(B.M, A.M);
+        }
+
+        public NMMatrix Inverse()
+        {
+            if (N != M) throw new Exception("NMMatrix.Inverse: matrix must be square");
+            return I(N) / this;
+        }
+
+        public double Max()
+        {
+            double max=double.MinValue;
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    max = Math.Max(_matrix[i, j], max);
+            return max;
+        }
+
+        public NMMatrix Apply(F func)
+        {
+            NMMatrix A = new NMMatrix(this);
+            for (int i = 0; i < A.N; i++)
+                for (int j = 0; j < A.M; j++)
+                    A[i, j] = func(A[i, j]);
+            return A;
+        }
+
+        public string ToString(string format)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append(Environment.NewLine);
+                for (int j = 0; j < M; j++)
+                    sb.Append(" " + this[i, j].ToString(format));
+            }
+            return sb.ToString();
+        }
+
+        private NMMatrix GaussJordanElimination()
+        {
+            double determinant = 1D;
+            for (int m = 0; m < Math.Min(N, M); m++)
+            {
+                //find largest element in this column, below pivot 
+                int r = m;
+                double max = Math.Abs(this[m, m]);
+                for (int i = m + 1; i < this.N; i++)
+                    if (Math.Abs(this[i, m]) > max)
+                    {
+                        r = i;
+                        max = Math.Abs(this[i, m]);
+                    }
+                //exchange rows to move to pivot location
+                determinant = r == m ? determinant : -determinant;
+                ExchangeRow(r, m);
+
+                //Divide row m by pivot
+                double p = this[m, m];
+                if (p == 0D) throw new Exception("GaussJordan: poorly formed system, no unique solution");
+                determinant *= p;
+                for (int j = m; j < M; j++)
+                    this[m, j] /= p;
+                for (int i = 0; i < N; i++)
+                {
+                    if (i == m) continue;
+                    p = this[i,m];
+                    for (int j = m; j < M; j++)
+                        this[i, j] -= p * this[m, j];
+                    determinant *= p;
+                }
+            }
+            return this;
+        }
+
+        private void ExchangeRow(int p, int q)
+        {
+            if (p == q) return;
+            double t;
+            for (int j = 0; j < _m; j++)
+            {
+                t = this[p, j];
+                this[p, j] = this[q, j];
+                this[q, j] = t;
+            }
+        }
+    }
+}
