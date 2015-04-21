e85445b3b631d94f6e9ef8d10a0dadf8e69df8c3
James E Lenz <jlenz@virginia.edu>
2015-04-20 21:15:33 -0400

    Minor editing of comments

--- CCILibrary/EventDictionary.cs
+++ CCILibrary/EventDictionary.cs
@@ -31,16 +31,19 @@ namespace EventDictionary
         private string m_description;
         public string Description { get { return m_description; } set { m_description = value; } } //need for binding
         public bool? intrinsic = true; //specifies the Event type: 
-            // intrinsic (true) are computer generated; extrinsic are external (nonsynchronous); use null for intrinsic Events with no Status marker
+            // intrinsic (true) are computer generated; extrinsic are external (nonsynchronous); use null for intrinsic Events with no Status marker (naked)
         public string IE { get { return intrinsic != null ? (bool)intrinsic ? "I" : "E" : "*"; } }
         public string channelName;
-        public int channel = -1; //specifies channel number that contains the extrinsic Event data (AIB)
+        public int channel = -1; //specifies channel number that contains the extrinsic Event data (AIB) -- only used for extrinsic Events
         public bool rise = false; //specifies for extrinsic Event whether event is nominally on rising (true) or falling edge of signal
         public bool location = false; // specifies for extrinsic Event whether analog signal "leads" (false) or "lags" (true) the Status event
         public double channelMax = 0; //specifies for extrinsic Event nominal maximum of the signal in channel
         public double channelMin = 0; //specifies for extrinsic Event nominal minimum of the signal in channel
         public List<GVEntry> GroupVars; //GroupVars in this Event
         public int ancillarySize = 0;
+
+        //There is no public constructor other than the default; the Name property is set at the time of addition to the Dictionary
+        // to assure match with the Key in the Dictionary
 
         public override string ToString()
         {
