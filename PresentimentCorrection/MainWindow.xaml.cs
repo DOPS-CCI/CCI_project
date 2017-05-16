7a51bb7e1799e929f82f3223a2958d70eafb0bdb
James E Lenz <jlenz@virginia.edu>
2017-05-15 21:54:35 -0400

    Upgraded to new Event structure and StatusChannel

--- PresentimentCorrection/MainWindow.xaml.cs
+++ PresentimentCorrection/MainWindow.xaml.cs
@@ -31,8 +31,12 @@ namespace PresentimentCorrection
             InitializeComponent();
         }
 
+
+        //Here's where the work is done
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //Convert parameters
             double v;
             try
             {
@@ -46,10 +50,13 @@ namespace PresentimentCorrection
                 System.Windows.MessageBox.Show("Invalid parameter; try again", "Invalid parameter", MessageBoxButton.OK);
                 return;
             }
+
             ExitButton.IsEnabled = false;
             DoIt.IsEnabled = false;
             Results.Text = "";
             RecordCounter = 0;
+
+            //Open dataset header
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.Title = "Open dataset for correction ...";
             dlg.AddExtension = true;
@@ -69,6 +76,7 @@ namespace PresentimentCorrection
             //make changes in Header file
             head = hfr.read();
 
+            //check to see that it's a Presentiment file
             EventDictionaryEntry ede;
             EventDictionaryEntry edeDownstairs = null; 
             EventDictionaryEntry edeUpstairs = null;
@@ -79,16 +87,15 @@ namespace PresentimentCorrection
                 System.Windows.MessageBox.Show("Not a valid Presentiment dataset", "Invalid dataset", MessageBoxButton.OK);
                 return;
             }
-            edeDownstairs.BDFBased = true; //make new Events using relative clock
-            edeUpstairs.BDFBased = true;
+            edeDownstairs.RelativeTime = true; //change Events to relative clock; they remain covered
+            edeUpstairs.RelativeTime = true;
 
-            //check to see if dataset may contain bad artifact markings
+            //check to see if dataset may contain bad artifact markings: they should have relative clocking, not absolute
             EventDictionaryEntry edeArtifactBegin = null;
             EventDictionaryEntry edeArtifactEnd = null;
             if (head.Events.TryGetValue("**ArtifactBegin", out ede)) edeArtifactBegin = ede;
             if (head.Events.TryGetValue("**ArtifactEnd", out ede)) edeArtifactEnd = ede;
-            bool containsBadArtifacts = edeArtifactBegin != null && edeArtifactEnd != null && !(edeArtifactBegin.BDFBased && edeArtifactEnd.BDFBased);
-
+            bool containsBadArtifacts = (edeArtifactBegin != null && edeArtifactBegin.HasAbsoluteTime) || (edeArtifactEnd != null && edeArtifactEnd.HasAbsoluteTime);
             string EventFilePath = System.IO.Path.Combine(directory, head.EventFile); //save old Event file address
 
             //set up BDF file to read
@@ -97,21 +104,23 @@ namespace PresentimentCorrection
             bdf.setExtrinsicChannelNumber(edeUpstairs); //don't forget to calculate the referenced extrinsic channel numbers!
             bdf.setExtrinsicChannelNumber(edeDownstairs);
 
+            StatusChannel sc = new StatusChannel(bdf, head.Status, false); //acquire Status channel information
+
             //Create the old and new Event files
             efr = new EventFileReader(new FileStream(EventFilePath, FileMode.Open, FileAccess.Read));
 
-            BDFLocFactory factory = new BDFLocFactory(bdf);
-            BDFLoc filePointer = factory.New();
-            BDFLoc p = factory.New();
+            BDFLoc p = bdf.LocationFactory.New();
 
             List<OutputEvent> outputList = new List<OutputEvent>();
             List<InputEvent> badArtifactRecords = new List<InputEvent>();
             bool foundInitialCoveredEvent = false;
-            double initialZeroTime = 0D;
+            double? initialZeroTime = null;
             OutputEvent oeTrialOnset;
             OutputEvent oeTargetDisplayed = null;
             EventFactory f = EventFactory.Instance(head.Events); //must set up Event factory before creation of Events permitted
             GrayCode gc = new GrayCode(head.Status);
+
+            Event.Event.LinkEventsToDataset(head, bdf); //link up input dataset
 
             foreach (InputEvent ie in efr)
             {
@@ -121,72 +130,69 @@ namespace PresentimentCorrection
                     continue;
                 }
 
-                oeTrialOnset = new OutputEvent(ie);
-                if (ie.BDFBased) oeTrialOnset.BDFTime = ie.Time; //Event.Time is already relative time, either covered or naked
-                else if (ie.IsCovered) //need to find its Status mark to determine relative time for absolute clock Event
+                //setRelativeTime in output Event
+                ie.setRelativeTime(sc);
+                oeTrialOnset = new OutputEvent(ie); //create new OutputEvent
+
+                if (ie.IsCovered && ie.HasAbsoluteTime) //need to find its Status mark to determine relative time for absolute clock Event
                 {
-                    do
+                    double t = sc.FindGCTime(ie.GC)[0]; //offset from start of BDF to Status mark for this Event; we assume that the GCs are unique
+
+                    bdf.setZeroTime(ie.Time - t); //update zeroTime (for naked absolute Events :-{ ); uses most recent absolute covered Event to synch clocks
+                    if (!foundInitialCoveredEvent) //need to save inital for bad Artifact Events
                     {
-                        if (!bdf.findNextGC(ref filePointer, ref gc, head.Status))
-                            throw new Exception("Premature end-of-file; unable to find TrialOnset Status marker " + ie.GC.ToString("0"));
-                    } while ((int)((GrayCode)gc).Value != ie.GC);
-                    double t = filePointer.ToSecs(); //offset from start of BDF to Status mark for this Event
-                    bdf.setZeroTime(ie.Time - t); //update zeroTime in case we need it (for naked absolute Events :-{ )
-                    if (!foundInitialCoveredEvent) { initialZeroTime = bdf.zeroTime; foundInitialCoveredEvent = true; }
-                    oeTrialOnset.BDFTime = t;
+                        initialZeroTime = bdf.zeroTime;
+                        foundInitialCoveredEvent = true;
+                    }
                 }
-                else //Event.Time is uncovered absolute; use estimate of offset from BDF, now based on last found covered absolute Event!
-                    oeTrialOnset.BDFTime = ie.Time - bdf.zeroTime; //throws an exception if zeroTime not yet set
 
                 outputList.Add(oeTrialOnset);
                 if (ie.Name != "TrialOnset") continue; //no further processing of Events in input file that aren't TrialOnset
 
                 //advance BDF scan to next Status mark after TrialOnset Event
-                //this should be the TargetDisplayedX Event of type implied by the value of GV "Target Location" in TrialOnset Event
-                if (!bdf.findNextGC(ref filePointer, ref gc, head.Status))
+                //this should be the TargetDisplayedXXX Event of type implied by the value of GV "Target Location" in TrialOnset Event
+                GCTime gct;
+                if (!sc.TryGetFirstGCTimeAfter(ie.relativeTime, out gct))
                     throw new Exception("Premature end-of-file");
-                int index = (int)((GrayCode)gc).Decode();
-                p = filePointer;
+                int index = (int)gct.GC.Decode();
+                p.FromSecs(gct.Time);
 
-                //make sure we can find a mark in the appropriate Ana channel
+                //now check for a mark in the appropriate Ana channel
                 switch (ie.GVValue[5])
                 {
                     case "Downstairs":
-                        if (findExtrinsicEvent(edeDownstairs, ref p)) //check to make sure there is a correct signal on Ana2
-                            oeTargetDisplayed = new OutputEvent(edeDownstairs, filePointer.ToSecs(), index); //if so create Event at the Status location 
-                        else
+                        oeTargetDisplayed = new OutputEvent(edeDownstairs, gct.Time, index); //if so create Event at the Status location 
+                        if (!findExtrinsicEvent(edeDownstairs, ref p)) //check to make sure there is a correct signal on Ana2
                             System.Windows.MessageBox.Show("Unable to find TargetDisplayedDownstair Ana2 signal for TrialOnset Event with index = " + ie.Index.ToString("0"),
-                                "WARNING: No TargetDisplayed event signal found", MessageBoxButton.OK);
+                                "WARNING: No TargetDisplayed extrinsic signal found", MessageBoxButton.OK);
                         break;
                     case "Upstairs":
-                        if (findExtrinsicEvent(edeUpstairs, ref p)) //check to make sure there is a correct signal on Ana3
-                            oeTargetDisplayed = new OutputEvent(edeUpstairs, filePointer.ToSecs(), index); //if so create Event at the Status location
-                        else
+                        oeTargetDisplayed = new OutputEvent(edeUpstairs, gct.Time, index); //if so create Event at the Status location
+                        if (!findExtrinsicEvent(edeUpstairs, ref p)) //check to make sure there is a correct signal on Ana3
                             System.Windows.MessageBox.Show("Unable to find TargetDisplayedUpstair Ana3 signal for TrialOnset Event with index = " + ie.Index.ToString("0"),
-                                "WARNING: No TargetDisplayed event signal found", MessageBoxButton.OK);
+                                "WARNING: No TargetDisplayed extrinsic signal found", MessageBoxButton.OK);
                         break;
                     case "None":
+                        oeTargetDisplayed = new OutputEvent(edeUpstairs, gct.Time, index); //if so create Event at the Status location
                         continue;
                     default:
-                        System.Windows.MessageBox.Show("Incorrect Target Location GV for Event " + index + "; value = " + ie.GVValue[5],
-                            "ERROR: Incompatable GV value", MessageBoxButton.OK);
+                        System.Windows.MessageBox.Show("Invalid Target Location GV for Event " + index + "; value = " + ie.GVValue[5],
+                            "ERROR: Invalid \"Target Location\" GV value", MessageBoxButton.OK);
+                        bdf.Close();
+                        efr.Close();
                         return;
                 }
 
-                //the problem here is that there might be a naked Event in the Event file between the covered TrialOnset and TargetDisplayedX Events.
-                //Applying a straightforward algorithm would place these Events out of order in the Event file.
-                //Should we build a routine to sort Events before we write them all out? This should be possible by adding a field to the internal Event
-                //objects that contains the BDF-based time of the Event; this works for all but naked absolute Events, but would require finding the Status
-                //mark for all covered absolute Events. These latter comments would not be significant in the case of this program however.
                 oeTargetDisplayed.GVValue = oeTrialOnset.GVValue; //have the same Group Variables
                 outputList.Add(oeTargetDisplayed); //so we save them in a list and order later
                 RecordCounter++;
             }
 
-            if(containsBadArtifacts)
+            //Now handle old Artifact Events, making them naked relative clock
+            if(containsBadArtifacts) //correct header file
             {
-                edeArtifactBegin.BDFBased=true;
-                edeArtifactEnd.BDFBased=true;
+                edeArtifactBegin.RelativeTime = true;
+                edeArtifactEnd.RelativeTime = true;
             }
             //create and save new Event file name in Header; write out Header
             head.EventFile = System.IO.Path.GetFileNameWithoutExtension(head.EventFile) + FilenameExtension.Text + System.IO.Path.GetExtension(head.EventFile);
@@ -196,18 +202,19 @@ namespace PresentimentCorrection
                     FileMode.Create, FileAccess.Write), head);
 
             foreach (InputEvent ie in badArtifactRecords)
-            {
+            { //initially naked absolute Events, referenced to first covered absolute Event; we assume that initialZeroTime is set
                 OutputEvent oe;
                 if (ie.Name == "**ArtifactBegin")
-                    oe = new OutputEvent(edeArtifactBegin, ie.Time - initialZeroTime);
+                    oe = new OutputEvent(edeArtifactBegin, ie.Time - (double)initialZeroTime);
                 else
-                    oe = new OutputEvent(edeArtifactEnd, ie.Time - initialZeroTime);
+                    oe = new OutputEvent(edeArtifactEnd, ie.Time - (double)initialZeroTime);
                 outputList.Add(oe);
             }
 
             //now we can sort them and write them out
-            outputList.Sort(); //using OutputEvent compare routine
+            outputList.Sort(); //using OutputEvent compare routine which uses best available relative time
 
+            //write out OutputEvents
             efw = new EventFileWriter(new FileStream(
                 System.IO.Path.Combine(directory, head.EventFile), FileMode.Create, FileAccess.Write));
             foreach (OutputEvent oe in outputList) efw.writeRecord(oe);
@@ -227,8 +234,9 @@ namespace PresentimentCorrection
         private bool findExtrinsicEvent(EventDictionaryEntry EDE, ref BDFLoc sp)
         {
             int rec = sp.Rec;
+            if (bdf.read(rec) == null) return false;
             int l = 0;
-            double th = (EDE.channelMax - EDE.channelMin) * threshold + EDE.channelMin;
+            double th = (EDE.channelMax - EDE.channelMin) * threshold + EDE.channelMin; //NB: this is only for positive-going ANA signals!
             do
             {
                 while (sp.Rec == rec)
