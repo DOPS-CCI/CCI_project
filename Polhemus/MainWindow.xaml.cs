8ebdd45bb1f91b2c1c7bb7a3ba37569579d86bdf
James E Lenz <jlenz@virginia.edu>
2017-08-02 21:14:10 -0400

    Minor maintenance

--- Polhemus/MainWindow.xaml.cs
+++ Polhemus/MainWindow.xaml.cs
@@ -1,30 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.ComponentModel;
 using System.IO;
-using System.IO.Ports;
 using System.Linq;
 using System.Media;
-using System.Resources;
 using System.Speech.Synthesis;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
-using ElectrodeFileStream;
 using CCIUtilities;
-using Polhemus;
+using ElectrodeFileStream;
 
 namespace Polhemus
 {
