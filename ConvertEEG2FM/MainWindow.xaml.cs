40ebe27f581f0f3dc36c0b7cf1dcd939fcaba86a
James E Lenz <jlenz@virginia.edu>
2017-11-17 12:59:30 -0500

    Added FM positioning; fixed bugs

--- ConvertEEG2FM/MainWindow.xaml.cs
+++ ConvertEEG2FM/MainWindow.xaml.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
-using System.IO;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Text.RegularExpressions;
+using FILMANFileStream;
 using MATFile;
 using MLTypes;
-using FILMANFileStream;
 
 namespace ConvertEEG2FM
 {
@@ -36,6 +30,7 @@ namespace ConvertEEG2FM
         int nGVs;
         string[] channels;
         string[] comments;
+        Task IOTask = null;
 
         public MainWindow()
         {
@@ -73,8 +68,20 @@ namespace ConvertEEG2FM
             NTrials.Text = nRecs.ToString("0");
             nPts = (int)(double)eeg.Select(prefix + "EEG.pnts"); //ND
             channels = new string[nChans];
+            MLType MLChans = (MLType)eeg.Select(prefix + "EEG.chanlocs");
             for (int i = 0; i < nChans; i++)
-                channels[i] = ((MLString)eeg.Select(prefix + "EEG.chanlocs[%].labels", i)).GetString();
+            {
+                string N = ((MLString)eeg.Select(MLChans, "[%].labels", i)).GetString();
+                if ((bool)IncludePosition.IsChecked)
+                {
+                    int phi = (int)Math.Round(90D - (double)eeg.Select(MLChans, "[%].sph_phi", i));
+                    int theta = (int)Math.Round((double)eeg.Select(MLChans, "[%].sph_theta", i));
+                    string P = String.Format("{0:0},{1:0}", phi, theta);
+                    channels[i] = N.PadRight(24 - P.Length) + P;
+                }
+                else
+                    channels[i] = N;
+            }
             string[] fn = ((MLStruct)eeg.Select(prefix + "EEG.event")).FieldNames;
             GVSelection.SelectedItem = null;
             GVSelection.ItemsSource = fn;
@@ -83,6 +90,7 @@ namespace ConvertEEG2FM
             StringBuilder sb = new StringBuilder();
             foreach (string s in com) sb.AppendLine(s);
             CommentText.Text = sb.ToString();
+            Convert.IsEnabled = true;
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
@@ -118,14 +126,15 @@ namespace ConvertEEG2FM
             //Create FILMAN file
             FILMANOutputStream fos = new FILMANOutputStream(
                 new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write),
-                nGVs + 1, 0, nChans, nPts, FILMANFileStream.FILMANFileStream.Format.Real);
+                nGVs + 2, 0, nChans, nPts, FILMANFileStream.FILMANFileStream.Format.Real);
             //Channel names
             for (int c = 0; c < nChans; c++)
                 fos.ChannelNames(c, channels[c]);
             //GV names
             fos.GVNames(0, "Channel");
+            fos.GVNames(1, "Montage");
             for (int g = 0; g < nGVs; g++)
-                fos.GVNames(g + 1, GVName[g]);
+                fos.GVNames(g + 2, GVName[g]);
             //Set sampling rate
             fos.IS = (int)Math.Round(srate);
             //Save description
@@ -139,26 +148,31 @@ namespace ConvertEEG2FM
             string datfile = ((MLString)eeg.Select(prefix + "EEG.datfile")).GetString();
             BinaryReader data = new BinaryReader(
                 new FileStream(System.IO.Path.Combine(directory, datfile), FileMode.Open, FileAccess.Read));
-            float[, ,] bigData = new float[nChans, nPts, nRecs];
-            for (int r = 0; r < nRecs; r++)
-                for (int pt = 0; pt < nPts; pt++)
-                    for (int c = 0; c < nChans; c++)
-                        bigData[c, pt, r] = data.ReadSingle();
-            data.Close();
 
+            IOTask = createNewFILMANFIle(fos, data, GVValue);
+        }
+
+        async Task createNewFILMANFIle(FILMANOutputStream fos, BinaryReader data, object[,] GVValue)
+        {
+            float[,] bigData = new float[nChans, nPts];
             for (int r = 0; r < nRecs; r++)
             {
+                //read in episode record (interleaved channel data)
+                for (int pt = 0; pt < nPts; pt++)
+                    for (int c = 0; c < nChans; c++)
+                        bigData[c, pt] = data.ReadSingle();
+                //handle GVs
                 for (int g = 0; g < nGVs; g++)
-                {
                     fos.record.GV[g + 1] = ConvertGV2Int(GVValue[r, g]);
-                }
+                //create FM record for each channel
                 for (int c = 0; c < nChans; c++)
                 {
                     for (int p = 0; p < nPts; p++)
-                        fos.record[p] = bigData[c, p, r];
+                        fos.record[p] = bigData[c, p];
                     fos.write();
                 }
             }
+            data.Close();
             fos.Close();
         }
 
@@ -171,7 +185,7 @@ namespace ConvertEEG2FM
                 int v;
                 try
                 {
-                    v = Convert.ToInt32(s);
+                    v = System.Convert.ToInt32(s);
                 }
                 catch (FormatException)
                 {
