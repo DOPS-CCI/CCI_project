83034eb06fc1eac0708787456e0b05c9d694033a
James E Lenz <jlenz@virginia.edu>
2018-10-20 17:15:22 -0400

    Minor clean-up

--- CCIUtilities/Utilities.cs
+++ CCIUtilities/Utilities.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Deployment.Application;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
