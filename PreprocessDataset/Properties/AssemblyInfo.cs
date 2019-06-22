05c801106084fa58b12c46fc4d448840e3fec82e
James E Lenz <jlenz@virginia.edu>
2019-06-22 13:09:59 -0400

    Prepare for uploading to server

--- PreprocessDataset/Properties/AssemblyInfo.cs
+++ PreprocessDataset/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RWNL")]
 [assembly: AssemblyProduct("PreprocessDataset")]
-[assembly: AssemblyCopyright("Copyright © CCI 2018")]
+[assembly: AssemblyCopyright("Copyright © CCI 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
