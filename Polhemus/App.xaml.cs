a02f8b73375ed84da940809ae56a7deede60781d
James E Lenz <jlenz@virginia.edu>
2019-10-15 16:30:56 -0400

    Add fail with info

--- Polhemus/App.xaml.cs
+++ Polhemus/App.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Threading;
+using CCIUtilities;
 
 namespace Polhemus
 {
@@ -12,5 +15,17 @@ namespace Polhemus
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+#if DEBUG
+            e.Handled = false;
+#else
+            Exception ex = e.Exception;
+            ErrorWindow ew = new ErrorWindow();
+            ew.Message = "Sender: " + sender.ToString() + "\r\nIn " + ex.TargetSite + ": " + ex.Message +
+                ";\r\n" + ex.StackTrace;
+            ew.ShowDialog();
+        }
+#endif
     }
 }
